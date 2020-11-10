0a971420021083b416e0ab0c0f700f8c09773160
zdeziel <zdeziel@korem.com>
2020-11-10 08:54:47 -0500

    added error handling

--- src/greetings/greetings.go
+++ src/greetings/greetings.go
@@ -1,10 +1,26 @@
 package greetings
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
+}
+
+// Hello returns a greeting for a named person
+func HelloError(name string) (string, error) {
+	// no name provided, throw an error
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// If a name provided, embed the name 
+	// in the greeting message
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message, nil
 }
---
+++ src/helloError/helloError.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	// Set properties of the predefined loggers
+	// includes the log entry prefix 
+	// and a flag to disable printing, 
+	// time, source file and line number
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// request a greeting message
+	message, err := greetings.HelloError("")
+
+	// if an error is returned, print it to console 
+	// and quit
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	// print message
+	fmt.Println(message)
+}
+
